352909e179f259daf7bead434de2c315bd59f494
wuwu <1427870484@qq.com>
2021-03-15 14:36:56 +0800

    test

---
+++ src/main/java/homeWork/day3/Test01.java
@@ -0,0 +1,13 @@
+package homeWork.day3;
+/**
+ * 使用Date输出当前系统时间，以及3天后这一刻的时间
+ * @author Bonnie
+ *
+ */
+public class Test01 {
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
+
+}
